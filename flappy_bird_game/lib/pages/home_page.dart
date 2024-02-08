46f95765e3be3dba63f4696397e123be3695c977
zaidnilawfer <zaidnilawfer@gmail.com>
2024-02-08 17:53:01 +0530

    home page background

--- flappy_bird_game/lib/pages/home_page.dart
+++ flappy_bird_game/lib/pages/home_page.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:flappy_bird_game/pages/developer_info_page.dart';
 import 'package:flappy_bird_game/pages/veiw_score_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,56 +10,65 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 123, 190, 125),
-      body: Center(
-        child: Column(children: [
-          const SizedBox(
-            height: 300,
+      backgroundColor: const Color.fromARGB(255, 123, 190, 125),
+
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_image.jpg'),
+            fit: BoxFit.cover
           ),
-          SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                  onPressed: (() => {}), child: const Text("Play "))),
-          const SizedBox(
-            height: 30,
           ),
-          SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ViewScorePage()),
-                    );
-                  },
-                  child: const Text('Veiw Scores'))),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                  onPressed: () => {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DeveloperInfoPage()))
-                      },
-                  child: const Text('Developer Info'))),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                  onPressed: () {
-                    exit(0);
-                  },
-                  child: const Text('Quit'))),
-          const SizedBox(
-            height: 30,
-          ),
-        ]),
+        child: Center(
+          child: Column(children: [
+            const SizedBox(
+              height: 300,
+            ),
+            SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: (() => {}), child: const Text("Play "))),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const ViewScorePage()),
+                      );
+                    },
+                    child: const Text('Veiw Scores'))),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const DeveloperInfoPage()))
+                        },
+                    child: const Text('Developer Info'))),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      exit(0);
+                    },
+                    child: const Text('Quit'))),
+            const SizedBox(
+              height: 30,
+            ),
+          ]),
+        ),
       ),
     );
   }
